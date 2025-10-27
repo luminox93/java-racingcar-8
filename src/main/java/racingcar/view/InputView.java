66bf1acb5171fc79f84159ac87c54911ab81461e
luminox93 <memoji1236@gmail.com>
2025-10-27 20:45:49 +0900

    refactor(view): 자동차 이름 파싱 로직을 InputView로 이동
    Controller에 있던 parseCarNames 메서드를 InputView로 이동하여
    입력 데이터 파싱 책임을 View 레이어로 위임했습니다.
    
    - InputView.readCarNames()가 List<String>을 직접 반환하도록 변경
    - 쉼표 구분 파싱 로직을 View 내부로 캡슐화
    - Controller는 파싱된 결과만 받아서 사용

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,24 @@
 package racingcar.view;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.view.messages.InputMessage;
 
 public class InputView {
 
-	public String readCarNames() {
+	public List<String> readCarNames() {
 		System.out.println(InputMessage.REQUEST_CAR_NAMES.getMessage());
-		return Console.readLine();
+		String input = Console.readLine();
+		return parseCarNames(input);
+	}
+
+	private List<String> parseCarNames(String input) {
+		return Arrays.stream(input.split(","))
+				.map(String::trim)
+				.collect(Collectors.toList());
 	}
 
 	public String readAttemptCount() {
